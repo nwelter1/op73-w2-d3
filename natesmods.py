f8b9e24143047e931edd1ebf9edc7517e5b86a43
nwelter1 <nwelter1@gmail.com>
2021-09-30 10:48:29 -0500

    hw

---
+++ natesmods.py
@@ -0,0 +1,11 @@
+import math
+
+def findArea(l,w):    
+#     l = int(input('What is the length of your house?'))
+#     w = int(input('What is the width?'))
+    return l*w
+
+def circumf(r):
+    return 2*math.pi*r
+
+
